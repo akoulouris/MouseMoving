fc79721eb4d7a947cbc78ed70d274bcd1f2fed54
Alexandros koulouris <Alexandros_koulouris@hotmail.com>
2018-04-02 01:43:34 -0400

    First commit

---
+++ MouseMoving/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MouseMoving
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(79, 71);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 28);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "If you feel lazy, just press Start";
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(226, 71);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(107, 28);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Go back to work";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 170);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stop;
+    }
+}
+
---
+++ MouseMoving/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace MouseMoving
+{
+    public partial class Form1 : Form
+    {
+
+        public const uint ES_CONTINUOUS = 0x80000000;
+        public const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        public const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint SetThreadExecutionState([In] uint esFlags);
+
+        static System.Drawing.Point TargetPoint;
+        static double Angle = 0d;
+
+        private TextBox textBox1;
+        private Cursor cursor;
+
+        bool exit = false;
+        private Keys keyData;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Lazy";
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+
+            SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+
+            Start.Enabled = false;
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            exit = false;
+            Task.Run(async () =>
+            {
+                while (!exit && keyData != Keys.Escape)
+                {
+                    // do the work in the loop
+                    string newData = DateTime.Now.ToLongTimeString();
+                    double Radius = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 3;
+                    Angle += Math.PI / 30;
+                    
+                    Cursor.Position = new System.Drawing.Point((int)(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 2 + Math.Cos(Angle) * Radius), (int)(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 2 + Math.Sin(Angle) * Radius));
+                    // update the UI on the UI thread
+                    //Dispatcher.Invoke(() => txtTicks.Text = "TASK - " + newData);
+
+                     //don't run again for at least 200 milliseconds
+                    await Task.Delay(10000);
+                }
+            });
+       
+        }
+       
+        
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Start.Enabled = true;
+            exit = true;
+            SetThreadExecutionState(ES_CONTINUOUS);
+        }
+    }
+}
